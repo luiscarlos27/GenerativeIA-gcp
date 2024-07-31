3560b9b6cbcdb35f61a0e2df228267c8a0175985
Carlos Vega <carlos.vega@detektor.com.gt>
2024-07-31 09:47:02 -0600

    Ajustes al ejemplo 1

--- ejemplo1.py
+++ ejemplo1.py
@@ -5,14 +5,14 @@ import ollama
 import chromadb
 
 documents = [
-  "La sangre de las llamas c"
-  "ontiene una gran cantidad de hemoglobina, la proteína que transporta el oxígeno de los pulmones a todo el cuerpo, lo que les permite sobrevivir a gran altura con bajos niveles de oxígeno.",
-  "Las llamas son rumiantes modificados con estómagos de tres cámaras que les permiten procesar una variedad de follaje en su entorno hostil",
-  "Las llamas tienen piel gruesa para mantenerlos calientes y ayudar a protegerlos contra las mordeduras de animales",
-  "Una velocidad de carrera máxima de 40 millas por hora (65 km/h) les ayuda a escapar de los depredadores.",
-  "Las hembras son más pequeñas que los machos.",
-  "Tienen cuellos y patas largos, cabezas relativamente pequeñas con el labio superior partido, orejas grandes y colas cortas.",
-  "Sus pies tienen dos dedos con uñas duras y una almohadilla correosa en la planta del dedo."
+  "El IPC desciende hasta el 3,2% en marzo en Guatemala 2024"
+  "La tasa de variación anual del IPC en Guatemala en marzo de 2024 ha sido del 3,2%,",
+  "1 décima inferior a la del mes anterior. La variación mensual del IPC (Índice de Precios al Consumo) ha sido del 0,3%,",
+  "de forma que la inflación acumulada en 2024 es del 0,5%.",
+  "Hay que destacar la subida del 2% de los precios de Transporte,",
+  "hasta situarse su tasa interanual en el 1,2%,",
+  "que contrasta con el descenso de los precios de Otros bienes y servicios del -0,1%,",
+  "y una variación interanual del 2,4%."
 ]
 
 client = chromadb.Client()
@@ -29,7 +29,7 @@ for i, d in enumerate(documents):
   )
 
   # an example prompt
-  prompt = "Brindame informacion sobre las llamas y su morfoligia?"
+  prompt = "Necesito informacion sobre el IPC de Guatemala del 2024?"
 
   # generate an embedding for the prompt and retrieve the most relevant doc
   response = ollama.embeddings(
@@ -45,7 +45,7 @@ for i, d in enumerate(documents):
   # generate a response combining the prompt and data we retrieved in step 2
   output = ollama.generate(
     model="phi3:mini",
-    prompt=f"Using this data: {data}. Respond to this prompt: {prompt}"
+    prompt=f"Usando estos datos: {data}. Responde este prompt: {prompt}"
   )
 
   print(output['response'])
